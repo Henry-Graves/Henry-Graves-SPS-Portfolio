f673c6588f4acc1a5d97ea67bd1b78a74d2b75f4
Henry <henrygraves11@yahoo.com>
2021-03-05 21:03:38 +0000

    Fix for previous commit's intended purpose: remove possibility of showing same message twice in a row to improve user experience. Uses localStorage.

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -1,51 +1,55 @@
-// Copyright 2020 Google LLC
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     https://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 // Adds a random hobby to the page.
 function showRandomHobby() {
-  const hobbies =
-      ['Doing Absolutely Nothing', 'Breathing', 'Forgetting to Blink', 'Tennis', 'Guitar', 'Walking in Nature', 
-      'Making Coffee', 'Reading Manga (ask me for recommendations!)', 'Calisthenics', 'Valorant', 'Learning Personal Finance', 
-      'Learning More about Software Development!'];
-  var resultHobby = " ";
+    const hobbies =
+        ['Doing Absolutely Nothing', 'Breathing', 'Forgetting to Blink', 'Tennis', 'Guitar', 'Walking in Nature', 
+         'Making Coffee', 'Reading Manga (ask me for recommendations!)', 'Calisthenics', 'Valorant', 'Learning Personal Finance', 
+         'Learning More about Software Development!'];
+     
+    // Pick a random hobby different from the last one, using localStorage to verify difference.     
+    var randomHobby = hobbies[Math.floor(Math.random() * hobbies.length)];
+    var resultHobby;
 
-  // Pick a random hobby different from the last one.
-  const randomHobby = hobbies[Math.floor(Math.random() * hobbies.length)];
+    if(localStorage.getItem('resultHobby') === undefined) {
+        resultHobby = randomHobby;
+        localStorage.setItem('resultHobby', resultHobby);
+    } 
+    else {
+        resultHobby = localStorage.getItem('resultHobby');
+    }
 
-  while (randomHobby == resultHobby) {
-        randomHobby = hobbies[Math.floor(Math.random() * strings.length)];
+    while (randomHobby === resultHobby) {
+        randomHobby = hobbies[Math.floor(Math.random() * hobbies.length)];
     }
     resultHobby = randomHobby;
+    localStorage.setItem('resultHobby', resultHobby);
 
-  // Add it to the page.
-  const hobbyContainer = document.getElementById('hobby-container');
-  hobbyContainer.innerText = resultHobby;
+    // Add it to the page.
+    const hobbyContainer = document.getElementById('hobby-container');
+    hobbyContainer.innerText = resultHobby;
 }
 
 // Fetches the hardcoded string from the server and adds it to the page.
 async function showRandomString() {
     const responseFromServer = await fetch('/string');
     const strings = await responseFromServer.json();
-    var resultString = " ";
 
-    // Pick a random string different from the last one.
-    const randomString = strings[Math.floor(Math.random() * strings.length)];
+    // Pick a random string different from the last one, using localStorage to verify difference.
+    var randomString = strings[Math.floor(Math.random() * strings.length)];
+    var resultString;
 
-    while (randomString == resultString) {
+    if(localStorage.getItem('resultString') === undefined) {
+        resultString = randomString;
+        localStorage.setItem('resultString', resultString);
+    } 
+    else {
+        resultString = localStorage.getItem('resultString');
+    }
+
+    while (randomString === resultString) {
         randomString = strings[Math.floor(Math.random() * strings.length)];
     }
     resultString = randomString;
+    localStorage.setItem('resultString', resultString);
 
     // Add it to the page
     const stringContainer = document.getElementById('string-container');
